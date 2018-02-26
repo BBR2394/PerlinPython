4c878a78983df88759818075ad59f818231817b3
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2018-02-26 23:14:03 +0100

    i am progressing, my random value tab generator seem to work, with the different step

--- perlin.py
+++ perlin.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python3
 import random
 
-def main(x, y, color, step):
+MasterCloud = []
+
+def extrapol():
+	print("nothing yet")
+
+def cloudGen(x, y, color, currentStep):
+
 	cloud = []
 	i = 0
-	stepCounter = 2
 	for j in range (y):
 		cloud.append([])
 		while i < x:
 			#cloud[j].append(random.randrange(255))
 			r = random.randrange(255)
-			print(r)
+			#print(r)
+			if currentStep > 1 and i > 0:
+				print("c'est le ", i,  " tours donc je vais extrapoler")
+				extrapol()
 			cloud[j].append(r)
-			i += stepCounter
+			i += currentStep
 		i = 0
-
 		print("bonjour")
 	print(cloud)
 	print("toto")
+	MasterCloud.append(cloud)
 
-main(16, 10, 1, 4)
+#step is for the master cloud 
+
+def perlinsCloud(x, y, color, step):
+	currentStep = 1
+	for i in range (step):
+		cloudGen(x, y, color, currentStep)
+		currentStep += 1
+
+	print("master cloud")
+	print(MasterCloud)
+
+	for j in range (step):
+		print("Master cloud step : ", j)
+		print(MasterCloud[j])
+
+perlinsCloud(16, 10, 1, 3)
